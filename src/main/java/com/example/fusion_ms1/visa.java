4920845987547b7482fd589ab48e40b462031360
Savita_Nalawade <spnalawade2000@gmail.com>
2024-10-06 00:35:15 +0530

    hello

---
+++ src/main/java/com/example/fusion_ms1/visa.java
@@ -0,0 +1,10 @@
+package com.example.fusion_ms1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class visa {
+    @GetMapping("/visa")
+    public String getData() {return  "Please take your visa" ; }
+}
